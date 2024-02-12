fa0e6b38ace6c940ab8324612c92a5b57c02ea94
Valentin Thevoz <valentin.thevoz@gmail.com>
2024-02-12 14:16:23 -0600

    Added Progress Bars

--- main.m
+++ main.m
@@ -14,10 +14,13 @@ archimedes_pi = zeros(Ns,1);
 archimedes_error = zeros(Ns,1);
 archimedes_time = zeros(Ns,1);
 
+bar = waitbar(0,'Archimedes Computing...');
 for i = 1:Ns
     [archimedes_pi(i), archimedes_error(i),...
         archimedes_time(i)] = my_archimedes(N(i));
+    waitbar(i/Ns);
 end
+close(bar);
 
 disp("Archimedes Computation is Finished!");
 
@@ -28,10 +31,13 @@ gregory_leibniz_pi = zeros(Ns,1);
 gregory_leibniz_error = zeros(Ns,1);
 gregory_leibniz_time = zeros(Ns,1);
 
+bar = waitbar(0,'Gregory Leibniz Computing...');
 for i = 1:Ns
     [gregory_leibniz_pi(i), gregory_leibniz_error(i),...
         gregory_leibniz_time(i)] = my_gregory_leibniz(N(i));
+    waitbar(i/Ns);
 end
+close(bar);
 
 disp("Gregory Leibniz Computation is Finished!");
 
@@ -42,10 +48,13 @@ mahadva_leibniz_pi = zeros(Ns, 1);
 mahadva_leibniz_error = zeros(Ns,1);
 mahadva_leibniz_time = zeros(Ns, 1);
 
+bar = waitbar(0,'Mahadva Leibniz Computing...');
 for i = 1:Ns
     [mahadva_leibniz_pi(i), mahadva_leibniz_error(i),...
         mahadva_leibniz_time(i)]  = my_mahadva_leibniz(N(i));
+    waitbar(i/Ns);
 end
+close(bar);
 
 disp("Mahadva Leibniz Computation is Finished!");
 
@@ -56,10 +65,13 @@ wallis_pi = zeros(Ns,1);
 wallis_error = zeros(Ns,1);
 wallis_time = zeros(Ns, 1);
 
+bar = waitbar(0,'Wallis Computing...');
 for i = 1:Ns
     [wallis_pi(i), wallis_error(i),...
         wallis_time(i)] = my_wallis(N(i));
+    waitbar(i/Ns);
 end
+close(bar);
 
 disp("Wallis Computation is Finished!");
 
@@ -70,10 +82,13 @@ euler_pi = zeros(Ns,1);
 euler_error = zeros(Ns,1);
 euler_time = zeros(Ns,1);
 
+bar = waitbar(0,'Euler Computing...');
 for i = 1:Ns
     [euler_pi(i), euler_error(i),...
         euler_time(i)] = my_euler(N(i));
+    waitbar(i/Ns);
 end
+close(bar);
 
 disp("Euler Computation is Finished!");
 
@@ -84,10 +99,13 @@ nilakantha_pi = zeros(Ns,1);
 nilakantha_error = zeros(Ns,1);
 nilakantha_time = zeros(Ns,1);
 
+bar = waitbar(0,'Nilakantha Computing...');
 for i = 1:Ns
     [nilakantha_pi(i), nilakantha_error(i),...
         nilakantha_time(i)] = my_nilakantha(N(i));
+    waitbar(i/Ns);
 end
+close(bar);
 
 disp("Nilakantha Computation is Finished!");
 
@@ -98,9 +116,12 @@ bbp_pi = zeros(Ns,1);
 bbp_error = zeros(Ns,1);
 bbp_time = zeros(Ns,1);
 
+bar = waitbar(0,'BBP Computing...');
 for i = 1:Ns
     [bbp_pi(i), bbp_error(i),...
         bbp_time(i)] = my_bbp(N(i));
+    waitbar(i/Ns);
 end
+close(bar);
 
 disp("BBP Computation is Finished!");
