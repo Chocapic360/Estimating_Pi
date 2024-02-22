11748fa1b6f617e6a1a9db7480267a2af42ef878
Valentin Thevoz <valentin.thevoz@gmail.com>
2024-02-21 19:14:48 -0600

    Fixed Wallis Aprroximation

--- my_wallis.m
+++ my_wallis.m
@@ -1,11 +1,11 @@
 function [pi_est, error, time] = my_wallis(N)
     tic
-    pi_est = 0;
+    pi_est = 1;
     for a = 1:N
         fun = ((2*a)^2)/(((2*a)^2) - 1);
-        pi_est = pi_est + fun;
+        pi_est = pi_est * fun;
     end
-    pi_est = 3*pi_est;
+    pi_est = 2*pi_est;
     error = abs(pi-pi_est);
     time = toc;
 end
